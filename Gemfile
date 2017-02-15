134c6231f235cb04daa68e418f6c7ebecf95db62
dany1468 <dany1468@gmail.com>
2017-02-15 21:26:06 +0900

    bundle gem

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in sidekiq-instantly_dead.gemspec
+gemspec
---
+++ lib/sidekiq/instantly_dead.rb
@@ -0,0 +1,7 @@
+require "sidekiq/instantly_dead/version"
+
+module Sidekiq
+  module InstantlyDead
+    # Your code goes here...
+  end
+end
