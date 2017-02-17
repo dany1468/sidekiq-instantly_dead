176152b47be5557dd6d465f898e2e135845eb280
dany1468 <dany1468@gmail.com>
2017-02-17 23:54:24 +0900

    Add spec

--- spec/sidekiq/instantly_dead_spec.rb
+++ spec/sidekiq/instantly_dead_spec.rb
@@ -1,11 +1,82 @@
-require 'spec_helper'
-
 describe Sidekiq::InstantlyDead do
-  it 'has a version number' do
-    expect(Sidekiq::InstantlyDead::VERSION).not_to be nil
+  class AWorker
+    include Sidekiq::Worker
+
+    def perform
+      raise 'error!'
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  class BWorker
+    include Sidekiq::Worker
+
+    def perform
+      raise Sidekiq::InstantlyDeadError
+    end
+  end
+
+  before do
+    allow(Redis).to receive(:new) { MockRedis.new }
+    Sidekiq.redis {|r| r.flushdb }
+
+    Sidekiq.server_middleware do |chain|
+      chain.insert_after Sidekiq::Middleware::Server::RetryJobs, Sidekiq::InstantlyDead::Middleware
+    end
+  end
+
+  let(:job_hash) { {class: worker.class, retry: 5}.stringify_keys }
+
+  subject { Sidekiq.server_middleware.invoke(worker, job_hash, 'default') { worker.perform } rescue nil }
+
+  context 'when raised RuntimeError' do
+    let(:worker) { AWorker.new }
+
+    specify do
+      expect { subject }.to change { Sidekiq::RetrySet.new.size }.from(0).to(1)
+    end
+
+    specify do
+      expect { subject }.not_to change { Sidekiq::DeadSet.new.size }
+    end
+
+    specify do
+      subject
+
+      expect(job_hash).to include({
+        class: AWorker,
+        retry: 5,
+        retry_count: 0,
+        queue: 'default',
+        error_message: 'error!',
+        error_class: 'RuntimeError',
+        failed_at: anything
+      }.stringify_keys)
+    end
+  end
+
+  context 'when raised InstantlyDeadError' do
+    let(:worker) { BWorker.new }
+
+    specify do
+      expect { subject }.not_to change { Sidekiq::RetrySet.new.size }
+    end
+
+    specify do
+      expect { subject }.to change { Sidekiq::DeadSet.new.size }.from(0).to(1)
+    end
+
+    specify do
+      subject
+
+      expect(job_hash).to include({
+        class: BWorker,
+        retry: 5,
+        retry_count: 6,
+        queue: 'default',
+        error_message: 'Sidekiq::InstantlyDeadError',
+        error_class: 'Sidekiq::InstantlyDeadError',
+        retried_at: anything
+      }.stringify_keys)
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,3 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'sidekiq/instantly_dead'
+require 'mock_redis'
